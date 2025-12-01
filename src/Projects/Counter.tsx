7c9b1228d426869d9d649215085400098ed69625
tanishq8000 <tanishqkhandelwal18072002@gmail.com>
2025-12-01 19:45:37 +0530

    Counter Project

--- src/Projects/Counter.tsx
+++ src/Projects/Counter.tsx
@@ -7,11 +7,28 @@ const Counter = () => {
     setCount(count + 1);
   };
 
+  const handleDecrement = () => {
+    setCount(count - 1);
+  };
+
   return (
-    <div>
-      <h1>Counter</h1>
-      <h3>{count}</h3>
-      <button onClick={handleIncrement}>Increment</button>
+    <div className="w-[200px] h-[200px] m-10 p-4 border border-amber-200 ">
+      <h1 className="text-center">Basic Counter</h1>
+      <h3 className="mt-3">Current count is : {count}</h3>
+      <div className="text-center mt-3">
+        <button
+          onClick={handleIncrement}
+          className="border border-black bg-green-300 m-2"
+        >
+          ➕
+        </button>
+        <button
+          onClick={handleDecrement}
+          className="border border-black bg-green-300"
+        >
+          ➖
+        </button>
+      </div>
     </div>
   );
 };
